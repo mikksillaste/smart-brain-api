c12877425b8393f9b2d0dacd69007012c8300fb3
mikksillaste <mikk.sillaste@gmail.com>
2018-11-16 22:29:47 +0200

    profile update

--- controllers/profile.js
+++ controllers/profile.js
@@ -14,9 +14,20 @@ const handleProfileGet = (req, res, db) => {
 const handleProfileUpdate = (req, res, db) => {
     const { id } = req.params;
     const { name, age, pet } = req.body.formInput;
-    
+    db('users')
+        .where({ id })
+        .update({ name })
+        .then(resp => {
+            if(resp) {
+                res.json("success")
+            } else {
+                res.status(400).json('unable to update')
+            }
+        })
+        .catch(err => res.status(400).json('error updating user'))
 };
 
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 };
