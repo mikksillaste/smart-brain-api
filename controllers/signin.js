d9cbc07f1abb2e3fc1f146d3ea77122cd48c2fc3
mikksillaste <mikk.sillaste@gmail.com>
2018-11-21 07:27:11 +0200

    Authentication

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const handleSignin = (db, bcrypt, req, res) => {
   const { email, password } = req.body;
   if (!email || !password) {
@@ -23,11 +25,25 @@ const getAuthTokenId = () => {
   console.log('Auth ok');
 };
 
+const signToken = (email) => {
+    const jwtPayload = { email };
+    //'JWT_SECRET' sould be something from environmental variables in real app e.g. process.env.JWTSECRET
+    return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
+};
+
+const createSessions = (data) => {
+    //create JWT token, return user data
+    const { email, id } = user;
+    const token = signToken(email);
+};
+
 const signinAuthentication = (db, bycrypt) => (req, res) => {
     const { authorization } = req.headers;
     return authorization ? getAuthTokenId() :
         handleSignin(db, bycrypt, req, res)
-            .then(data => res.json(data))
+            .then(data => {
+                data.id && data.email ? createSessions(data) : Promise.reject(data)
+            })
             .catch(err => res.status(400).json(err))
 };
 
